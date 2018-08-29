0c9795851f27c34bc9dbbf92f3629ea417d3fe4c
Kishikawa Katsumi <kkishikawa@folio-sec.com>
2018-08-30 07:24:55 +0900

    Initial Commit

---
+++ Folio-UI-CollectionTests/Folio_UI_CollectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Folio_UI_CollectionTests.swift
+//  Folio-UI-CollectionTests
+//
+//  Created by kkishikawa on 2018/08/30.
+//  Copyright © 2018 FOLIO Co.,Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import Folio_UI_Collection
+
+class Folio_UI_CollectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
