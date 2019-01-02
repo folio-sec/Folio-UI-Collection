b1942a91d467ef6971bca580b549b746d8898bcf
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2019-01-02 18:36:16 +0900

    Add IGListKit

--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@ use_frameworks!
 target 'Folio-UI-Collection' do
   pod 'RxSwift'
   pod 'RxCocoa'
+  pod 'IGListKit'
 
   target 'Folio-UI-CollectionTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-Folio-UI-Collection/Pods-Folio-UI-Collection-frameworks.sh
+++ Pods/Target Support Files/Pods-Folio-UI-Collection/Pods-Folio-UI-Collection-frameworks.sh
@@ -148,11 +148,13 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/IGListKit/IGListKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/IGListKit/IGListKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxAtomic/RxAtomic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
